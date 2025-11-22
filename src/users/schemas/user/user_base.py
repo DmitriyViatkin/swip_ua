9ffc8305460ec878c0a9842cf9c6b3db5e2f19a6
DimaViatkin <Viatkindima@gmail.com>
2025-11-22 10:27:25 +0200

    Endpoint User

--- src/users/schemas/user/user_base.py
+++ src/users/schemas/user/user_base.py
@@ -1,6 +1,7 @@
 from pydantic import BaseModel, EmailStr, Field, ConfigDict
 from src.enums import UserRole
 from typing import Optional
+#from src.users.schemas.notification.base import NotificationBase
 
 class UserBase(BaseModel):
     first_name: Optional[str] = Field(None, max_length=50)
@@ -8,6 +9,7 @@ class UserBase(BaseModel):
     phone: Optional[str] = Field(None, max_length=20)
     email: Optional[EmailStr] = None
     role: UserRole = UserRole.CLIENT
+   # notification: "NotificationBase"
 
     model_config = ConfigDict(
         from_attributes=True,
