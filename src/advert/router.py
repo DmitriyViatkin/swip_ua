fe6afa3b00629f99c932d9795c13ea491e94c565
Dmitriy <ViatkinDima@gmail.com>
2026-03-06 08:58:17 +0200

    Fix Data Base3

--- src/advert/router.py
+++ src/advert/router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from .routers.create_adverts_routers import router as create_advert
 from .routers.update_advers_routers import router as update_advert
+from .routers.adverts_by_user import router as user_advert
 from .routers.delete_adverts_routers import router as delete_advert
 from .routers.all_adverts_routers import router as all_advert
 from .routers.get_advert_by_id import router as advert_by_id
@@ -14,6 +15,7 @@ router=APIRouter(prefix="/adverts", tags=["Adverts"])
 # Подключаем все подроутеры
 
 router.include_router(all_advert)
+router.include_router(user_advert)
 router.include_router(create_advert)
 router.include_router(advert_by_id)
 router.include_router(update_advert)
